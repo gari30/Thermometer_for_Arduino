5968b3311395b3d0f88f22dae08e02cce524ef11
Takahashi Yoshiki <gariyoshi0630@gmail.com>
2017-02-02 03:01:34 +0900

    add.

--- Thermometer/seg_set.ino
+++ Thermometer/seg_set.ino
@@ -3,165 +3,38 @@
 namespace Thermometer {
 
 namespace {
-void seg_disp0(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,LOW);
-  digitalWrite(seg7_f,LOW);
-  digitalWrite(seg7_g,HIGH);
-  digitalWrite(seg7_dp,HIGH);
+
+void seg_clear(void) {
+  PORTD = 0xff;
 }
 
-void seg_disp1(void)
-{
-  digitalWrite(seg7_a,HIGH);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,HIGH);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,HIGH);
-  digitalWrite(seg7_g,HIGH);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp2(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,HIGH);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,LOW);
-  digitalWrite(seg7_f,HIGH);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp3(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,HIGH);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp4(void)
-{
-  digitalWrite(seg7_a,HIGH);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,HIGH);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,LOW);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp5(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,HIGH);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,LOW);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp6(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,HIGH);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,LOW);
-  digitalWrite(seg7_f,LOW);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-
-void seg_disp7(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,HIGH);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,HIGH);
-  digitalWrite(seg7_g,HIGH);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp8(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,LOW);
-  digitalWrite(seg7_f,LOW);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_disp9(void)
-{
-  digitalWrite(seg7_a,LOW);
-  digitalWrite(seg7_b,LOW);
-  digitalWrite(seg7_c,LOW);
-  digitalWrite(seg7_d,LOW);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,LOW);
-  digitalWrite(seg7_g,LOW);
-  digitalWrite(seg7_dp,HIGH);
-}
-
-void seg_clear(void)
-{
-  digitalWrite(seg7_a,HIGH);
-  digitalWrite(seg7_b,HIGH);
-  digitalWrite(seg7_c,HIGH);
-  digitalWrite(seg7_d,HIGH);
-  digitalWrite(seg7_e,HIGH);
-  digitalWrite(seg7_f,HIGH);
-  digitalWrite(seg7_g,HIGH);
-  digitalWrite(seg7_dp,HIGH);
-}
+constexpr unsigned char display_type[] = {
+  0xc0, //0
+  0xf9, //1
+  0xa4, //2
+  0xb0, //3
+  0x99, //4
+  0x92, //5
+  0x82, //6
+  0xf8, //7
+  0x80, //8
+  0x90, //9
+};
 
 }  // namespace
 
 void seg_disp::ThermoMain() {
   auto a = 1000;
-  while (1) {
-    seg_disp0();
+  int count = 0;
+  while (true) {
+    count += 1;
+/*    if (PORTB == 0x01) {
+      PORTD = display_type[count % 10];
+    }else {
+      PORTD = display_type[count / 10];
+    }*/
+    PORTD = display_type[count % 10];
     delay(a);
-    seg_disp1();
-    delay(a);
-    seg_disp2();
-    delay(a);
-    seg_disp3();
-    delay(a);
-    seg_disp4();
-    delay(a);
-    seg_disp5();
-    delay(a);
-    seg_disp6();
-    delay(a);
-    seg_disp7();
-    delay(a);
-    seg_disp8();
-    delay(a);
-    seg_disp9();
-    delay(a);
-    seg_clear();
   }
 }
 
